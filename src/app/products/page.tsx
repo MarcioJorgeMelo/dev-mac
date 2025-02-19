cfb2fcf0decea06c9dfea459e544e189e8b5281a
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-02-19 13:28:27 -0300

    feat: define initial routes

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1>Página produto</h1>
+      <Button>Clique aqui!</Button>
+      <Input placeholder="Boraaa" />
+    </div>
+  );
+};
+
+export default ProductsPage;

88ed8616bfa86979b6156d23a9332cabc8303a94
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2025-02-24 03:06:33 -0300

    feat: reajusting parameters

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,39 +1,47 @@
-"use client"
+"use client";
 import { formatCurrency } from "@/utils/format-currency";
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 
 interface ProductsProps {
-    products: Product[];
+  products: Product[];
 }
 
 const Products = ({ products }: ProductsProps) => {
-    const {slug} = useParams<{slug: string}>();
+  const { slug } = useParams<{ slug: string }>();
+  const searchParams = useSearchParams();
+  const consumptionMethod = searchParams.get("consumptionMethod");
 
-    return (
-        <div className="space-y-3 px-5">
-            {products.map(product => (
-                <Link key={product.id} href={`/${slug}/menu/${product.id}`} className="flex items-center justify-between gap-10 py-3 border-b">
-                    <div>
-                        <h3 className="text-sm font-medium">{product.name}</h3>
-                        <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
-                        <p>{formatCurrency(product.price)}</p>
-                    </div>
+  return (
+    <div className="space-y-3 px-5">
+      {products.map((product) => (
+        <Link
+          key={product.id}
+          href={`/${slug}/menu/${product.id}?consumptionMethod=${consumptionMethod}`}
+          className="flex items-center justify-between gap-10 border-b py-3"
+        >
+          <div>
+            <h3 className="text-sm font-medium">{product.name}</h3>
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p>{formatCurrency(product.price)}</p>
+          </div>
 
-                    <div className="relative min-h-[82px] min-w-[120px]">
-                        <Image
-                            src={product.imageUrl}
-                            alt={product.name}
-                            fill
-                            className="rounded-lg object-contain"
-                        />
-                    </div>
-                </Link>
-            ))}
-        </div>
-    );
-}
+          <div className="relative min-h-[82px] min-w-[120px]">
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              fill
+              className="rounded-lg object-contain"
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default Products;
